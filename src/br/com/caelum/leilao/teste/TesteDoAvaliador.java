483aa91057dd1634d073ee138942fdaa8f1392eb
lucasdeassis <lucasassis413@gmail.com>
2017-09-25 21:24:07 -0300

    adiciona classes de negócio, classe de servió de avaliador e testes.

---
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,34 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+	
+	@Test
+	public void testeAvalia() {
+		Usuario joao = new Usuario("joão");
+		Usuario jose = new Usuario("josé");
+		Usuario maria = new Usuario("maria");
+
+		Leilao leilao = new Leilao("Playstation 3 novo");
+
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+
+		double maiorEsperado = 400.0;
+		double menorEsperado = 250.0;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+}
